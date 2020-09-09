01de0fae8b805280ea275d6a47279b60794fd47e
Viktor Ödman <vo222dq@student.lnu.se>
2020-09-09 13:17:45 +0200

    Adding Date and Time

--- view/DateTimeView.php
+++ view/DateTimeView.php
@@ -4,9 +4,16 @@ class DateTimeView {
 
 
 	public function show() {
+		date_default_timezone_set('Europe/Stockholm');
+		$dateTime = new DateTime('now');
 
-		$timeString = 'TODO, Write servertime here...';
+		$dayText = date('l');
+		$dayNumber = date('jS');
+		$monthAndYear = date('F Y');
+		$timeHoursMinutesSeconds = date('H:i:s');
 
-		return '<p>' . $timeString . '</p>';
+		$timeString = "{$dayText}, The {$dayNumber} of {$monthAndYear}, The time is {$timeHoursMinutesSeconds}";
+
+		return "<p>{$timeString}</p>";
 	}
 }
